af98b6bf516d344cb842fe9b8082d9cb2ad183d6
Erich Henrique <erich.hs13@gmail.com>
2023-04-17 15:29:18 -0700

    Test core

---
+++ tests/test_compare.py
@@ -0,0 +1,65 @@
+import numpy as np
+import pandas as pd
+from tabularcompare import compare
+
+# Test parameters
+df1 = pd.DataFrame(
+    {"idx1": [1, 1, 2, 3, 3, 4],
+     "idx2": ["A", "B", "A", "A", "B", "A"],
+     "fname": ["Nikola", "Nikola", "Albert", "Stephen", "Stephen", "Steve"],
+     "lname": ["Tesla", "Tesla", "Einstein", "Hawking", "Hawking", "Jobs"],
+     "emptycol": [pd.NA for i in range(6)],
+     "email": ["em@tesla.com", "em@spacex.com", "ae@gmail.com", "sh@cam.uk", pd.NA, "sj@apple.com"],
+     "dob": ["1856-07-10", "1856-07-10", "1879-03-14", "1942-01-08", "1942-01-08", "1955-02-24"],
+     "active": [True, False, True, True, False, True]}
+)
+df1["dob"] = pd.to_datetime(df1["dob"])
+df2 = pd.DataFrame(
+    {"idx1": [1, 1, 2, 3, 3, 2],
+     "idx2": ["A", "B", "A", "A", "B", "A"],
+     "fname": ["Nick", "Nick", "Albert", "Stephen", "Stephen", "Albert"],
+     "lname": ["Tesla", "Tesla", "Einstein", "Hawking", "Hawking", "Einstein"],
+     "emptycol": [pd.NA for i in range(6)],
+     "email": ["em@tesla.com", "em@spacex.com", "ae@gmail.com", "sh@cam.uk", pd.NA, "ae@hotmail.com"],
+     "dob": ["1856-07-10", pd.NA, "1879-03-14", "1942-03-08", "1942-01-08", "1879-03-14"],
+     "active": [True, False, True, True, False, pd.NA]}
+)
+df2["dob"] = pd.to_datetime(df2["dob"])
+expected_partial_report = 'DataComPy Comparison\n--------------------\n\nDataFrame Summary\n-----------------\n\n  DataFrame  Columns  Rows\n0       df1        8     6\n1       df2        8     6\n\nColumn Summary\n--------------\n\nNumber of columns in common: 8\nNumber of columns in df1 but not in df2: 0\nNumber of columns in df2 but not in df1: 0\n\nRow Summary\n-----------\n\nMatched on: idx1, idx2\nAny duplicates on match values: Yes\nAbsolute Tolerance: 0\nRelative Tolerance: 0\nNumber of rows in common: 5\nNumber of rows in df1 but not in df2: 1\nNumber of rows in df2 but not in df1: 1\n\nNumber of rows with some compared columns unequal: 3\nNumber of rows with all compared columns equal: 2\n\nColumn Comparison\n-----------------\n\nNumber of columns compared with some values unequal: 2\nNumber of columns compared with all values equal: 6\nTotal number of values which compare unequal: 4\n\nColumns with Unequal Values or Types\n------------------------------------'
+expected_diverging_subset = pd.DataFrame(
+    {'idx1': {0: 1, 1: 1, 3: 3},
+     'idx2': {0: 'A', 1: 'B', 3: 'A'},
+     'fname': {0: '{Nikola} --> {Nick}', 1: '{Nikola} --> {Nick}', 3: np.nan},
+     'dob': {0: np.nan,
+             1: '{1856-07-10} --> {NaT}',
+             3: '{1942-01-08} --> {1942-03-08}'}}
+)
+expected_df1_unq_columns = pd.DataFrame(
+    {'idx1': {0: 1, 1: 1, 2: 2, 3: 3, 4: 3, 5: 4},
+     'idx2': {0: 'A', 1: 'B', 2: 'A', 3: 'A', 4: 'B', 5: 'A'}}
+)
+expected_df2_unq_columns = pd.DataFrame(
+    {'idx1': {0: 1, 1: 1, 2: 2, 3: 3, 4: 3, 5: 2},
+     'idx2': {0: 'A', 1: 'B', 2: 'A', 3: 'A', 4: 'B', 5: 'A'}}
+)
+expected_df1_unq_rows = pd.DataFrame(
+    {'idx1': {5: 4},
+     'idx2': {5: 'A'},
+     'fname': {5: 'Steve'},
+     'lname': {5: 'Jobs'},
+     'emptycol': {5: ""},
+     'email': {5: 'sj@apple.com'},
+     'dob': {5: pd.Timestamp('1955-02-24 00:00:00')},
+     'active': {5: True}}
+)
+expected_df2_unq_rows = pd.DataFrame(
+    {'idx1': {6: 2},
+     'idx2': {6: 'A'},
+     'fname': {6: 'Albert'},
+     'lname': {6: 'Einstein'},
+     'emptycol': {6: ""},
+     'email': {6: 'ae@hotmail.com'},
+     'dob': {6: pd.Timestamp('1879-03-14 00:00:00')},
+     'active': {6: ""}}
+)
+expected_intersect_columns = ['idx1', 'idx2', 'fname', 'lname', 'emptycol', 'email', 'dob', 'active']
